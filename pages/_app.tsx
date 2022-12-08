3ffbb1de70fab1231f1a1f7b850e8b208621cacc
Culic96 <nikola.culic96@gmail.com>
2022-12-08 11:10:10 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../public/styles/global.css'
+// import type { AppProps } from 'next/app'
+import Index from './Index'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App() {
+  return (
+    <>
+      <Index />
+    </>
+  )
 }
